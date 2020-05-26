b0e6130b2cedbe9e137432ad0f5a1b56b73dedc8
Brenyn <brenynk@gmail.com>
2020-05-25 20:17:22 -0400

    convert netfilter packets into scapy packets so we can use scapy filters

--- dns_spoofer.py
+++ dns_spoofer.py
@@ -13,10 +13,12 @@
 #
 ##########################################################################################################
 import netfilterqueue
-
+import scapy.all as scapy
 
 def process_packet(packet):
-	print(packet)
+	scapy_packet = scapy.IP(packet.get_payload()) #convert netfilterqueue packet into scapy packet so we can use scapy filters
+	print(scapy_packet.show())
+	packet.accept()
 
 queue = netfilterqueue.NetfilterQueue()
 queue.bind(0, process_packet)
